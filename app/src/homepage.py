cef03e6294cd8ee7001d2c12deb792ef0633b79c
balakrishnaakuleti <bakuleti@microsoft.com>
2025-01-21 14:56:06 +0530

    updates

--- app/src/homepage.py
+++ app/src/homepage.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from chat_bot import show_chat_app
-from services.azure_aad_service import get_authorization_url, get_token_from_code, get_user_profile
+from services.azure_aad_service import get_authorization_url, get_user_profile
 
 # Streamlit app UI
 st.title("Welcome to the Question and Answers Chatbot on news articles!!!")
@@ -23,12 +23,3 @@ else:
         auth_url = get_authorization_url()
         st.markdown(f'<a href="{auth_url}" target="_self">Please login using this link</a>', unsafe_allow_html=True)
 
-    # Handle the redirect (After login in Azure, Azure will redirect back to this URI with code)
-    code = st.query_params.get("code")
-    if code is not None:
-        result = get_token_from_code(code[0])
-        if "access_token" in result:
-            st.session_state.access_token = result['access_token']
-            st.rerun()
-        else:
-            st.write("Error: " + result.get("error_description", "Unknown error"))
