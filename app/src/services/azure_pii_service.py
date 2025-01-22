8baa3e2d9444fc485b0e4f46639f71d7312e0604
balakrishnaakuleti <bakuleti@microsoft.com>
2025-01-22 09:23:54 +0530

    Redact only specific PII

--- app/src/services/azure_pii_service.py
+++ app/src/services/azure_pii_service.py
@@ -5,7 +5,18 @@ from azure.core.credentials import AzureKeyCredential
 from azure.identity import DefaultAzureCredential
 from azure.keyvault.secrets import SecretClient
 from app.src.util import constants
+from azure.ai.textanalytics import PiiEntityCategory
 
+# PII entities to be redacted
+PII_ENTITITIES = [
+                  PiiEntityCategory.EMAIL.value, 
+                  PiiEntityCategory.URL.value,
+                  PiiEntityCategory.AGE.value,
+                  PiiEntityCategory.PHONE_NUMBER.value,
+                  PiiEntityCategory.IP_ADDRESS.value,
+                  PiiEntityCategory.PERSON.value,
+                  PiiEntityCategory.ADDRESS.value
+                ]
 # Secrets from keyvault
 credential = DefaultAzureCredential()
 client = SecretClient(vault_url=constants.KEY_VAULT_URL, credential=credential)
@@ -23,7 +34,7 @@ client = authenticate_client()
 
 # Example method for detecting sensitive information (PII) from text 
 def redact_pii(query):
-    result = client.recognize_pii_entities([query])
+    result = client.recognize_pii_entities(documents=[query], categories_filter=PII_ENTITITIES)
     docs = [doc for doc in result if not doc.is_error]
     #Only one document so, return the first document's redacted text
     return docs[0].redacted_text
